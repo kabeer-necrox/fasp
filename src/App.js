e294d7796acf9b1c08ae81f66059c5585c5f2177
kabeer-necrox <khushkabeer@gmail.com>
2023-08-18 17:07:07 +0300

    this is the toggle code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import react, { useState } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [print, setPrint] = useState(false);
-  function getData(val) {
-    console.log(val.target.value);
-    setData(val.target.value);
-  }
+ const [status, setStatus] = react.useState(true);
+  
   return (
     <div>
-      {
-        print?
-        <h1>data</h1>
-        :null
-      }
-      <h1>{data}</h1>
-      <input
-        type="text"
-        placeholder="enter your data here"
-        onChange={getData}
-      />
-      <button onClick={()=> setPrint(true)}>print data</button>
+     {
+      status? <h1>hello world!!!</h1>:null
+     }
+
+     <button  onClick={()=>setStatus(!true)}>toggle</button>
     </div>
   );
 }
